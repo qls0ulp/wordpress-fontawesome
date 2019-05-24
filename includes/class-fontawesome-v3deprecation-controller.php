2cca9fbcc0a4201de6711a2661cd0dfa2f5f5613
Mike Wilkerson <mwilkerson@gmail.com>
2019-05-24 15:38:58 -0700

    Clean up ini_set calls that phpcs hates

--- includes/class-fontawesome-v3deprecation-controller.php
+++ includes/class-fontawesome-v3deprecation-controller.php
@@ -97,9 +97,6 @@ if ( ! class_exists( 'FontAwesome_V3Deprecation_Controller' ) ) :
 			// Ideally, we'd be able to detect which plugin results in such an error by catching it and then
 			// reporting to the client which plugin caused the error. But at a minimum, we need to make sure
 			// that we return 500 instead of 200 in these cases.
-			// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.runtime_configuration_ini_set
-			ini_set( 'display_errors', 0 );
-
 			try {
 				$fa = fa();
 
@@ -121,9 +118,6 @@ if ( ! class_exists( 'FontAwesome_V3Deprecation_Controller' ) ) :
 		 * @return WP_Error|WP_REST_Response
 		 */
 		public function update_item( $request ) {
-			// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.runtime_configuration_ini_set
-			ini_set( 'display_errors', 0 );
-
 			try {
 				$fa = fa();
 
