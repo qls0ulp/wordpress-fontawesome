842a292d733184d6060bc7bbf76160f965279d55
Mike Wilkerson <mwilkerson@gmail.com>
2019-06-06 10:37:07 -0700

    Make ErrorFallbackView a simpler functional component

--- admin/src/ErrorFallbackView.js
+++ admin/src/ErrorFallbackView.js
@@ -1,36 +1,38 @@
 import React from 'react'
 import styles from './ErrorFallbackView.module.css'
 import Alert from './Alert'
+import PropTypes from 'prop-types'
 
-class ErrorFallbackView extends React.Component {
+function ErrorFallbackView(props) {
+  const { message } = props
 
-  render() {
-    const { message } = this.props
-
-    return <div className={ styles['error-fallback'] }>
-      <Alert title='Whoops, this is embarrassing' type='warning'>
-        <p>
-          Some unexpected error has occurred.
-        </p>
-        <p>
-          You might be able to find a solution to this
-          problem in the <a href="https://wordpress.org/support/plugin/font-awesome/" rel="noopener noreferrer" target="_blank">plugin's support forum</a>.
-          If not, you could post about the problem there.
-        </p>
-        {
-          message &&
-          <div>
-            Please include the following if you make an error report:
-            <div className={ styles['additional-message'] }>
-              <p>
-                { message }
-              </p>
-            </div>
+  return <div className={ styles['error-fallback'] }>
+    <Alert title='Whoops, this is embarrassing' type='warning'>
+      <p>
+        Some unexpected error has occurred.
+      </p>
+      <p>
+        You might be able to find a solution to this
+        problem in the <a href="https://wordpress.org/support/plugin/font-awesome/" rel="noopener noreferrer" target="_blank">plugin's support forum</a>.
+        If not, you could post about the problem there.
+      </p>
+      {
+        message &&
+        <div>
+          Please include the following if you make an error report:
+          <div className={ styles['additional-message'] }>
+            <p>
+              { message }
+            </p>
           </div>
-        }
-      </Alert>
-    </div>
-  }
+        </div>
+      }
+    </Alert>
+  </div>
+}
+
+ErrorFallbackView.propTypes = {
+  message: PropTypes.string
 }
 
 export default ErrorFallbackView
