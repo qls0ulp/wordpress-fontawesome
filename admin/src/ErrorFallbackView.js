976cd6805f68032b61916ba8df057a0590ec68dc
Mike Wilkerson <mwilkerson@gmail.com>
2019-06-06 10:34:45 -0700

    WIP: more Alert refactoring

--- admin/src/ErrorFallbackView.js
+++ admin/src/ErrorFallbackView.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import styles from './ErrorFallbackView.module.css'
-import sharedStyles from './App.module.css'
-import classnames from 'classnames'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+import Alert from './Alert'
 
 class ErrorFallbackView extends React.Component {
 
@@ -11,37 +8,27 @@ class ErrorFallbackView extends React.Component {
     const { message } = this.props
 
     return <div className={ styles['error-fallback'] }>
-      <div className={ classnames(sharedStyles['alert'], sharedStyles['alert-warning']) } role="alert">
-        <div className={ sharedStyles['alert-icon'] }>
-          <FontAwesomeIcon icon={ faExclamationTriangle } size='lg' fixedWidth />
-        </div>
-        <div className={ sharedStyles['alert-message'] }>
-          <h2 className={ sharedStyles['alert-title'] }>
-            Whoops, this is embarrassing.
-          </h2>
-          <div className={ sharedStyles['alert-copy'] }>
-            <p>
-              Some unexpected error has occurred.
-            </p>
-            <p>
-              You might be able to find a solution to this
-              problem in the <a href="https://wordpress.org/support/plugin/font-awesome/" rel="noopener noreferrer" target="_blank">plugin's support forum</a>.
-              If not, you could post about the problem there.
-            </p>
-            {
-              message &&
-              <div>
-                Please include the following if you make an error report:
-                <div className={ styles['additional-message'] }>
-                  <p>
-                    { message }
-                  </p>
-                </div>
-              </div>
-            }
+      <Alert title='Whoops, this is embarrassing' type='warning'>
+        <p>
+          Some unexpected error has occurred.
+        </p>
+        <p>
+          You might be able to find a solution to this
+          problem in the <a href="https://wordpress.org/support/plugin/font-awesome/" rel="noopener noreferrer" target="_blank">plugin's support forum</a>.
+          If not, you could post about the problem there.
+        </p>
+        {
+          message &&
+          <div>
+            Please include the following if you make an error report:
+            <div className={ styles['additional-message'] }>
+              <p>
+                { message }
+              </p>
+            </div>
           </div>
-        </div>
-      </div>
+        }
+      </Alert>
     </div>
   }
 }
